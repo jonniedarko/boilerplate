cdb417f5768b5621d4e6f6b1b6d19642b41f768f
John Maguire <jonnie.maguire@gmail.com>
2015-12-20 23:02:01 -0500

    adding bootstrap

--- gulpFile.js
+++ gulpFile.js
@@ -4,15 +4,20 @@ var gulp = require('gulp');
 var browserify = require('./gulp/browserify');
 var html = require('./gulp/copy-html');
 var nodemon = require('./gulp/nodemon');
+var less = require('./gulp/less');
 
 // Tasks
-gulp.task('build-browserify', browserify.build)
-gulp.task('watch-browserify', browserify.watch)
+gulp.task('build-browserify', browserify.build);
+gulp.task('watch-browserify', browserify.watch);
+
+gulp.task('less', less.build);
+gulp.task('watch-less', less.watch);
+
 
 gulp.task('watch-html', html.watch);
 gulp.task('copy-html', html.copy);
 
-gulp.task('watch', ['watch-html', 'watch-browserify']);
+gulp.task('watch', ['watch-html','watch-less', 'watch-browserify']);
 
 gulp.task('start', nodemon.dev)
 
