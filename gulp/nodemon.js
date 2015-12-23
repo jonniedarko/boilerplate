622b889533d0c6414720aae4598644adf64dccd8
John <johnm@swirl.com>
2015-12-23 10:56:56 -0500

    cleaned up dependencies & exit nodemon task cleanly

--- gulp/nodemon.js
+++ gulp/nodemon.js
@@ -1,14 +1,18 @@
 var nodemon = require('gulp-nodemon');
 
 function dev() {
-  nodemon({
-    execMap: {
-      js: 'node-inspector & node --debug'
-    },
-    script: 'index.js',
-  //, ext: 'js html'
-    watch: './server',
-    env: { 'NODE_ENV': 'development' }
-  });
+    nodemon({
+        execMap: {
+            js: 'node-inspector & node --debug'
+        },
+        script: 'index.js',
+        //, ext: 'js html'
+        watch: './server',
+        env: {
+            'NODE_ENV': 'development'
+        }
+    }).once('quit', function() {
+        process.exit()
+    });
 }
 module.exports.dev = dev;
